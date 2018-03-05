2584b93fd31516800a7156370e6fb61279298ca4
Babu Thomas <babuthomas138@gmail.com>
2018-03-05 10:36:08 +0530

    Create blog models and apply migrations

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
