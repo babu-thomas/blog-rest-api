c4c283f624a634315b1165ffcd21a9ed5ebc6d42
Babu Thomas <babuthomas138@gmail.com>
2018-03-05 11:05:42 +0530

    Configure urls and views for GETs

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class BlogList(generics.ListAPIView):
+    queryset = models.Blog.objects.all()
+    serializer_class = serializers.BlogSerializer
+
+
+class BlogDetail(generics.RetrieveAPIView):
+    queryset = models.Blog.objects.all()
+    serializer_class = serializers.BlogSerializer
